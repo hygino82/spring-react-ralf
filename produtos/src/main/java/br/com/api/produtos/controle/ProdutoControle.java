f0cef129d3a80db687543beff0edc90b401d09aa
Adroaldo <hygino@msn.com>
2022-09-02 18:23:16 -0300

    Criado controle, serviço e repositório

---
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -0,0 +1,7 @@
+package br.com.api.produtos.controle;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProdutoControle {
+}
---
+++ produtos/src/main/java/br/com/api/produtos/modelo/RespostaModelo.java
@@ -0,0 +1,4 @@
+package br.com.api.produtos.modelo;
+
+public class RespostaModelo {
+}
